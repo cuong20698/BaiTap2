1b5057146676674de10e43fc8ca58d4fecace886
cuong20698 <vovietcuongcntt39b@gmail.com>
2019-07-18 10:24:53 +0700

    cập nhật Bài tập 2
    tạo và code cho Class UserDAO

---
+++ DangKyThanhVien/DangKyThanhVien/UserDAO.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace DangKyThanhVien
+{
+    public class UserDAO
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString;
+
+        public bool CheckUser(string userName)
+        {
+            string sql = @"SELECT COUNT(*) FROM UserInfo WHERE UserName = @usn";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(sql, connection);
+                command.Parameters.AddWithValue("@usn", userName);
+                connection.Open();
+                int count = (int)command.ExecuteScalar();
+                return (count >= 1);
+            }
+        }
+
+        public bool Insert(User user)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = @"INSERT INTO UserInfo VALUES(@usn, @pass, @fname, @lname, @email, @gender, @address)";
+                SqlCommand command = new SqlCommand(sql, connection);
+                command.Parameters.AddWithValue("@usn", user.UserName);
+                command.Parameters.AddWithValue("@pass", user.PassWord);
+                command.Parameters.AddWithValue("@fname", user.FirstName);
+                command.Parameters.AddWithValue("@lname", user.LastName);
+                command.Parameters.AddWithValue("@email", user.Email);
+                command.Parameters.AddWithValue("@gender", user.Gender);
+                command.Parameters.AddWithValue("@address", user.Address);
+
+                connection.Open();
+                int count = command.ExecuteNonQuery();
+                return (count >= 1);
+            }
+        }
+    }
+}
